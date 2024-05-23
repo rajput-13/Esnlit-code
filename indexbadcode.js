f15656731c7d99c5b32f7881e7887a1c7cb4d3d9
rajput-13 <thakurditto@gmail.com>
2024-05-23 08:59:36 +0530

    added: name change bad code

---
+++ indexbadcode.js
@@ -0,0 +1,41 @@
+ // eg 1
+
+// var  name = "vikram singh"
+// var  email = "vikramthakur093@hotmail.com"
+
+// var employee = {
+//     name:  name,
+//     email: email
+// }
+
+// eg 2  use   const
+
+// const  name = "vikram singh"
+// const email = "vikramthakur093@hotmail.com"
+
+// const  employee = {
+//     name:  name,
+//     email: email
+// }
+
+// eg 3  use single '  '
+
+// const  name = 'vikram singh';
+// const email = 'vikramthakur093@hotmail.com';
+
+// const  employee = {
+//     name:  name,
+//     email: email
+// }
+
+// eg  4  error kam krna hai 
+const  name = 'vikram singh';
+const email = 'vikramthakur093@hotmail.com';
+const address = '505 not found' // future mei address add kiya ha / krna hai 
+
+const  employee = {
+    name,  
+    email,
+    address,
+}
+
