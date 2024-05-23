4baeb7ac21fd67b4949ec537ff516359054af634
rajput-13 <thakurditto@gmail.com>
2024-05-23 08:41:41 +0530

    added: files

---
+++ README.md
@@ -0,0 +1,8 @@
+    1. Getting Started with ESLint
+
+ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs.
+
+2. Quick start
+
+npm init @eslint/config@latest
+
---
+++ index.js
@@ -0,0 +1,29 @@
+ // eg 1
+
+// var  name = "vikram singh"
+// var  email = "vikramthakur093@hotmail.com"
+
+// var employee = {
+//     name:  name,
+//     email: email
+// }
+
+// eg 2  use   const
+
+// const  name = "vikram singh"
+// const email = "vikramthakur093@hotmail.com"
+
+// const  employee = {
+//     name:  name,
+//     email: email
+// }
+
+// eg 3  use single '  '
+
+const  name = 'vikram singh';
+const email = 'vikramthakur093@hotmail.com';
+
+const  employee = {
+    name:  name,
+    email: email
+}
